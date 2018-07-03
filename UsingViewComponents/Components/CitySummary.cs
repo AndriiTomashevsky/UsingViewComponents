39894b2a30973b069f7902d89340b7f865499706
AndriiTomashevsky <tomashevskiy.a@gmail.com>
2018-07-03 23:06:53 +0300

    Understanding View Component Results

---
+++ UsingViewComponents/Components/CitySummary.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using UsingViewComponents.Models;
+
+namespace UsingViewComponents.Components
+{
+    public class CitySummary : ViewComponent
+    {
+        private ICityRepository repository;
+
+        public CitySummary(ICityRepository repo)
+        {
+            repository = repo;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(new CityViewModel
+            {
+                Cities = repository.Cities.Count(),
+                Population = repository.Cities.Sum(c => c.Population)
+            });
+        }
+    }
+}
