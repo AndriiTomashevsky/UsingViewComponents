fd0c731913f53fbfec333bd1315d2de81bbd52dd
AndriiTomashevsky <tomashevskiy.a@gmail.com>
2018-07-03 22:40:10 +0300

    Add project files.

---
+++ UsingViewComponents/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UsingViewComponents.Models;
+
+namespace UsingViewComponents.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IProductRepository repository;
+
+        public HomeController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index()
+        {
+            return View(repository.Products);
+        }
+
+        public ViewResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product newProduct)
+        {
+            repository.AddProduct(newProduct);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
